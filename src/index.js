e0110a1b91fd276c9271dd9f0bd6044267f3f141
M. Kaspul Anwar <154990308+mkaspulanwar@users.noreply.github.com>
2025-08-27 15:18:06 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -36,7 +36,7 @@ const ASSETS = [
         icon: "icon/bitcoin.png",
         type: "bitcoin",
         currency: "IDR",
-        freeSats: 6712 + 49781 - 10000, //10000 sats punya APT
+        freeSats: 7796 + 49781 - 10000, //10000 sats punya APT
         avgPrice: 1166095031,
         currentPrice: 1500000000, // Harga default Bitcoin dalam IDR
         apiId: "bitcoin" // ID untuk API CoinGecko
